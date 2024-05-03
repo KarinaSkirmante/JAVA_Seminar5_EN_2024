fa405097235b07914d9212dcdc675e502327daac
KarinaSkirmante <karina.krinkele@gmail.com>
2024-05-03 15:08:52 +0300

    Add: create functionality for Product

--- src/main/java/lv/venta/controller/ProductCRUDController.java
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lv.venta.model.Product;
 import lv.venta.service.IProductCRUDService;
@@ -36,7 +38,7 @@ public class ProductCRUDController {
 	}
 		
 	}
-	//localhost:8080/product/crud?id=1
+	
 	
 	
 
@@ -56,12 +58,42 @@ public class ProductCRUDController {
 	}
 	
 	
+	@GetMapping("/one")//localhost:8080/product/crud/one?id=1
+	public String getProductCrudByIdWithQuestionmark(@RequestParam("id")int id, Model model) {
+		
+		try
+		{
+			Product result = productCRUDservice.retrieveById(id);
+			model.addAttribute("mypackage", result);
+			return "show-one-product-page";
+		}
+		catch (Exception e) {
+			model.addAttribute("mypackage", e.getMessage());
+			return "error-page";//will show error-page.html page with exception message
+		}
+	}
 	
 	
+	@GetMapping("/create") //localhost:8080/product/crud/create
+	public String getProductCRUDCreate(Model model) {
+		model.addAttribute("product", new Product());
+		return "create-product-page";//will show reate-product-page.html page with default product
+	}
 	
-	
-	
-	
+	@PostMapping("/create")
+	public String postproductCRUDCreate(Product product, Model model)
+	{
+		try {
+			productCRUDservice.create(product.getTitle(), product.getDescription(),
+					product.getPrice(), product.getQuantity());
+			return "redirect:/product/crud/all";//the endpoint localhost:8080/product/crud/all will be called
+			
+		} catch (Exception e) {
+			model.addAttribute("mypackage", e.getMessage());
+			return "error-page";//will show error-page.html page with exception message
+		}
+		
+	}
 	
 	
 
